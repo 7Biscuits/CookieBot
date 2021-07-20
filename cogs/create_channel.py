d743b931efe284f5618fd0af903250ffdb1a0404
BiscuitButB <RudranshSrivastava2525@gmail.com>
2021-07-20 13:25:23 +0530

    Update create_channel.py

--- cogs/create_channel.py
+++ cogs/create_channel.py
@@ -6,19 +6,20 @@ class ModCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=['createtextchannel', 'createtxtch', 'ctextch', 'addtextch])
     @commands.has_permissions(manage_channels=True)
-    async def textch(self, ctx, channel: discord.TextChannel=None):
+    async def addtextchannel(self, ctx, channel=None):
         if channel is not None:
-            await ctx.guild.create_text_channel(channel=channel)
+            await ctx.guild.create_text_channel(name='{}'.format(channel))
             embed = discord.Embed(
-                title="New channel created",
+                title="New text channel created",
                 description=f'{channel} has been created',
                 colour=discord.Colour.green()
             )
-            embed.add_field(name="Channel: ", value=f'{channel.mention}')
+            embed.add_field(name="Channel: ", value=f'{channel}')
             embed.add_field(name='Moderator: ', value=f'{ctx.author.name}')
             embed.set_thumbnail(url=f"{ctx.author.avatar_url}")
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(ModCommands(bot))
